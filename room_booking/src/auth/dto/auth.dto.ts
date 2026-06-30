3ae9536f2f07fb2aaee2abb7a6e60db5cb2f84ae
BabickijDV <dimababickij50@gmail.com>
2026-06-30 11:31:19 +0300

    Update auth.dto.ts

--- room_booking/src/auth/dto/auth.dto.ts
+++ room_booking/src/auth/dto/auth.dto.ts
@@ -3,34 +3,34 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class RegisterDto {
   @IsEmail()
-  email: string;
+  email!: string;
 
   @IsString()
   @MinLength(8)
-  password: string;
+  password!: string;
 
   @IsString()
   @MinLength(2)
   @MaxLength(50)
-  firstName: string;
+  firstName!: string;
 
   @IsString()
   @MinLength(2)
   @MaxLength(50)
-  lastName: string;
+  lastName!: string;
 }
 
 export class LoginDto {
   @IsEmail()
-  email: string;
+  email!: string;
 
   @IsString()
   @MinLength(1)
-  password: string;
+  password!: string;
 }
 
 export class RefreshTokenDto {
   @ApiProperty({ description: 'Refresh токен' })
   @IsString()
-  refreshToken: string;
+  refreshToken!: string;
 }
